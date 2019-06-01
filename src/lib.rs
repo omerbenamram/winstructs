dde5cf7b4515aeb4b43aab8612678633ccade18a
Omer Ben-Amram <omerbenamram@gmail.com>
2019-06-01 17:00:51 +0300

    (cargo-release) start next development iteration 0.2.1

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,11 @@
 //! `from_buffer` is also provided as convenience, but it generally just builds a cursor and uses
 //! either `from_reader` or `from_stream` internally.
 //!
+#![deny(unused_must_use)]
+#![deny(unsafe_code)]
+// Don't allow dbg! prints in release.
+#![cfg_attr(not(debug_assertions), deny(clippy::dbg_macro))]
+
 use std::io::{self, Read, Seek, SeekFrom};
 
 #[macro_use]
